ceaa9cf15bdfcd6768fc1d77b61b32de73fd4ebd
erica-r05 <reginaerica073@gmail.com>
2026-08-08 16:54:12 -0300

    adiciona o ambiente backend e as configurações do gitignore

---
+++ docs/back/doceDeleite/src/main/java/com/confeitaria/doceDeleite/DoceDeleiteApplication.java
@@ -0,0 +1,13 @@
+package com.confeitaria.doceDeleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoceDeleiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceDeleiteApplication.class, args);
+	}
+
+}
